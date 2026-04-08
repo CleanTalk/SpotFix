af6be25ca9bfe0167f74fdbe6e9bd63db257d918
Veronika Tseleva <nika.tseleva@cleantalk.org>
2026-04-09 01:49:27 +0400

    Fix. fixed style

--- js/src/loaders/SpotFixTemplatesLoader.js
+++ js/src/loaders/SpotFixTemplatesLoader.js
@@ -188,7 +188,7 @@ class SpotFixTemplatesLoader {
                         <button id="doboard_task_widget-register_only_button" class="doboard_task_widget-submit_button doboard_task_widget-hidden" style="margin-top: 8px; width: 100%;">Sign up</button>
         
                         <i>Note about DoBoard register and accepting email notifications about tasks have to be here.</i>
-                        </br>
+                        
                         <i>If you are a doBoard user, use same Email and password as at <a href="https://doboard.com" target="_blank" rel="nofollow">doboard.com</a>
                             on the <span id="doboard_task_widget-show_login_form" class="doboard_task_widget-show_login_form">login page</span>
                          </i>
